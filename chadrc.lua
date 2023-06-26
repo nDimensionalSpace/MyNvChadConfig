8a647d11307acd4d0d43160566b13d17039df2f7
Neil Hodge <neil.hodge@gmail.com>
2023-06-26 16:51:46 -0700

    made wombat the default

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+  theme = "wombat",
+  theme_toggle = { "wombat", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
